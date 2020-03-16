3db5c8d2e2e5995a4b67441bc7a9f5a248529f32
Gabriele D'Intino <gabriele.dintino@icloud.com>
2020-03-16 18:58:27 +0100

    Test commit Xcode

---
+++ HabitTracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HabitTracker
+//
+//  Created by Gabriele on 16/03/2020.
+//  Copyright © 2020 Gabriele. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Prova Git")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
